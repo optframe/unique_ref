e5a5f835f46b4ca063a9ba7ca565de55540d285e
Igor Machado <igor.machado@gmail.com>
2020-05-22 16:08:09 -0300

    'unique_ref' namespace and 'uref' class

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # unique_ref
 unique reference implementation in C++ (similar to unique_ptr)
+
+## How to use it
+
+Just `#include "unique_ptr.hpp"`, it's a single header library!
+
+You can import "unique_ptr" namespace `using unique_ptr::uref` to get `uref` class,
+or just use it like `unique_ptr::uref<MyClass> obj` to create `MyClass& obj` equivalent.
+
+What is the advantage over regular reference?
+
+This should be used when you cannot really stack alloc your object, but still don't want
+to use a pointer... if you use `uref` we will manage the memory for you (using `std::unique_ptr`
+  in the background).
+
+## License
+
+This project is MIT licensed, so feel free to use and share it!
+
+2020 
---
+++ unique_ref.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <memory>
+
+// Author: Igor M. Coelho
+// May 2020
+// unique_ref: an implementation of unique references (similar to std::unique_ptr)
+// The namespace is called 'unique_ref', but decided to name class 'uref' to keep name shorter
+// Please inform if there's any name conflict with "bigger" projects
+//   (if it's necessary to change it for some reason, file an Issue on 'https://github.com/optframe/unique_ref')
+//
+// MIT License
+
+//
+// declaring namespace 'unique_ref'
+//
+namespace unique_ref {
+//
+// ====================== uref class ========================
+// the only requirement here is a copy constructor for 'T'
+//
+// method 'ref()' returns the internal reference
+// operator-> gives pointer navigation to T fields
+// operator* gives a reference of T object
+// operator T() gives implicit copy construction of object
+//
+template<class T>
+class uref final // 'uref' is shorter than 'unique_ref'
+{
+private:
+   std::unique_ptr<T> _t;
+
+public:
+   explicit uref(const uref<T>& other) noexcept
+     : _t{ std::make_unique<T>(*other._t) }
+   {
+   }
+
+   explicit uref(const T& t) noexcept
+     : _t{ std::make_unique<T>(t) }
+   {
+   }
+
+   explicit uref(std::unique_ptr<T>&& uptr) noexcept
+     : _t{ uptr }
+   {
+   }
+
+   T& ref() { return *_t; }
+   const T& ref() const { return *_t; }
+
+   // returns pointer (beware to not break element!)
+   T* operator->() { return _t.get(); }
+   const T* operator->() const { return _t.get(); }
+
+   // returns reference (beware to not break element!)
+   T& operator*() { return *_t; }
+   const T& operator*() const { return *_t; }
+
+   // conversion copy
+   operator T() const { return std::move(T{ *_t }); } // returns copy/move
+};
+//
+} // namespace unique_ref
+//
+
+//
+// ============== SIMPLE USAGE ==============
+// Just create an object 'uref<YourType>' and this is
+//  "equivalent" to 'YourType&', with automatic memory management.
+//
+//
+// #include "unique_ref.hpp" // single header lib
+// using unique_ref::uref;   // gets class 'uref'
+//
+// class XYZ {
+//  ...
+//  uref<X> x;               // declares "reference" to 'X' (it's actually a pointer...)
+//
+//  XYZ(const X& _x) :       // constructor of XYZ must give an initialization for x object
+//    x{_x}
+//  {
+//   ...
+//  }
+//
+// That's it! Good luck.
+//
